39167aa0ee451706a14d018b3de28448243c12a6
HyuuunjuKim <gus7wn@gmail.com>
2020-03-11 14:38:26 +0900

    initial settings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, BroeserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import App1 from './components/App1';
+import App2 from './components/App2';
+import App3 from './components/App3';
+import App4 from './components/App4';
+import App5 from './components/App5';
+import App6 from './components/App6';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={App1}/>
+                    <Route exact path="/App1" component={App1}/>
+                    <Route exact path="/App2" component={App2}/>
+                    <Route exact path="/App3" component={App3}/>
+                    <Route exact path="/App4" component={App4}/>
+                    <Route exact path="/App5" component={App5}/>
+                    <Route exact path="/App6" component={App6}/>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
